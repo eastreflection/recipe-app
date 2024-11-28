36037f5745a8e2f3edbea9d9168e7a3c8353016b
Sujal Kale <76490094+eastreflection@users.noreply.github.com>
2024-11-28 16:32:19 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
